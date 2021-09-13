d1ad12f76128c9846dcc0731df531b6078bc87b9
Mikhail Kalashnikov <89983973+Mihromka@users.noreply.github.com>
2021-09-13 20:52:35 +0300

    Update Class1.cs

--- PasswordCheker/Class1.cs
+++ PasswordCheker/Class1.cs
@@ -1,1 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace ClassLibraryPassword
+{
+    public class PasswordChecker
+    {
+        public static bool ValidatePassword(string Password)
+        {
+            if (Password.Length < 8 || Password.Length > 20)
+                return false;
+                if (!Password.Any(Char.IsLower))
+                    return false;
+            if (!Password.Any(char.IsUpper))
+                return false;
+            if (!Password.Any(char.IsDigit))
+                return false;
+            if(Password.Intersect("#$%^&_").Count() == 0)
+             return false;
+            return true;
+        }
+    }
+}
