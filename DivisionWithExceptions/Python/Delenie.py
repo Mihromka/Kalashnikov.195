6c01232ed25b597c34353e3cfb8cef653d4dc520
Mikhail Kalashnikov <89983973+Mihromka@users.noreply.github.com>
2021-09-14 10:54:52 +0300

    Update Delenie.py

--- DivisionWithExceptions/Python/Delenie.py
+++ DivisionWithExceptions/Python/Delenie.py
@@ -1,1 +1,14 @@
-
+import sys
+print("Int size(bytes)" + str(sys.getsizeof(int)))
+print("float size(bytes)" + str(sys.getsizeof(float)))
+try:
+    num1 = int(input("Enter the first number:"))
+    num2 = int(input("Enter the second number:"))
+    print("Result:", num1/num2)
+except ValueError:
+    print("Not a number")
+except ZeroDivisionError:
+    print("You can't divide zero")
+except():
+    print("Global exception")
+print("The end")
