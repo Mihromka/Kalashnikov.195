a946ddd8b6d738c305236087e3fca3bfcc6a78f0
Mikhail Kalashnikov <89983973+Mihromka@users.noreply.github.com>
2021-09-14 10:38:06 +0300

    Update Delenie.cpp

--- DivisionWithExceptions/C++/Delenie.cpp
+++ DivisionWithExceptions/C++/Delenie.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
 
+double divide(int, int);
+
+int main()
+{
+	std::cout << "Enter the first number";
+	double num1;
+	std::cin >> num1;
+
+	std::cout << "Enter the second number";
+	double num2;
+	std::cin >> num2;
+	try
+	{
+	  double res = divide(num1, num2);
+		std::cout << res << std::endl;
+	}
+	catch (const std::exception& err)
+	{
+		std::cout << "Error!!!" << std::endl;
+	}
+	std::cout << "The End..." << std::endl;
+	return 0;
+}
+
+double divide(int num1, int num2)
+{
+	if (num2 == 0)
+		throw std::exception();
+	return num1 / num2;
+}
