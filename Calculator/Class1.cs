3a6868c2860d16536093be8d926541c84fa18cff
Mikhail Kalashnikov <89983973+Mihromka@users.noreply.github.com>
2021-09-21 10:01:34 +0300

    Update Class1.cs

--- Calculator/Class1.cs
+++ Calculator/Class1.cs
@@ -8,30 +8,70 @@ namespace calculator
 {
     public class CalculatorChecker
     {
-        public static bool ValidateCalculator(double sum1, double sum2, string operation, double result)
+        public static double ValidateCalculator(double sum1, double sum2, string operation)
         {
             if (operation == "+")
             {
-                return (sum1 + sum2 == result);
+                return (sum1 + sum2);
             }
-            
-            if(operation == "-")
+
+            if (operation == "-")
             {
-                return (sum1 - sum2 == result);
+                return sum1 - sum2;
 
             }
             if (operation == "*")
             {
-                return (sum1 * sum2 == result);
+                return (sum1 * sum2);
 
             }
             if (operation == "/")
             {
-                return (sum1 / sum2 == result);
+                if (sum2 == 0.0D)
+                {
+                    throw new DivideByZeroException();
 
+                    
+                }
+                return (sum1 / sum2);
+                try
+                {
+                    
+
+                    if (sum2 == 0)
+                    {
+                        Console.WriteLine("Вы не можете делить на ноль ");
+
+                        return 1;
+                    }
+                    else
+                        Console.WriteLine("Результат :" + " " + sum1 / sum2);
+
+                }
+                catch
+                {
+                    Console.WriteLine("Неправильный ввод");
+                }
+
+                
             }
-            return true;
+            
+            return 0;
         }
-        
+        static public void Main()
+        {
+            double a = 0;
+            double b = 0;
+
+
+
+            Console.WriteLine("Введите число 1");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите число 2");
+            b = Convert.ToDouble(Console.ReadLine());
+            double res = CalculatorChecker.ValidateCalculator(a, b, "*");
+            Console.WriteLine("res" + res);
+        }
+
     }
 }
